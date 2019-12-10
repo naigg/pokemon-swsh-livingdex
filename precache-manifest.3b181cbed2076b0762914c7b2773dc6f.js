c43094aeb70279a248baefb6e5f89fe89a85b73e
Gian Andres <gian.andres@gmail.com>
2019-12-10 12:31:00 +1100

    remove unused files

---
+++ precache-manifest.3b181cbed2076b0762914c7b2773dc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559c84d93987e39422c68f5511024728",
+    "url": "/pokemon-swsh-livingdex/index.html"
+  },
+  {
+    "revision": "30ecda990858078717be",
+    "url": "/pokemon-swsh-livingdex/static/css/main.03141d8c.chunk.css"
+  },
+  {
+    "revision": "8a6de2acf10a1df5970d",
+    "url": "/pokemon-swsh-livingdex/static/js/2.54f24ff5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokemon-swsh-livingdex/static/js/2.54f24ff5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30ecda990858078717be",
+    "url": "/pokemon-swsh-livingdex/static/js/main.40c34182.chunk.js"
+  },
+  {
+    "revision": "d7cb8525c968e77c91a6",
+    "url": "/pokemon-swsh-livingdex/static/js/runtime-main.c0b12281.js"
+  }
+]);

0611a848fb0bf75841b0fd8651fb519b26bd05b6
Gian Andres <gian.andres@gmail.com>
2019-12-10 12:57:10 +1100

    add a filter function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-import PokedexTracker from './components/pokedex-tracker'
+import PokedexTracker from "./components/pokedex-tracker";
 
-import pokdexJson from './helpers/data.json';
-import useLocalStorage from './helpers/use-local-storage-hook';
+import pokdexJson from "./helpers/data.json";
+import useLocalStorage from "./helpers/use-local-storage-hook";
 
 const defaultCaughtPokemonObject = [];
 
 function App() {
-  const [caughtPokemons, setCaughtPokemons] = useLocalStorage('caughtPokemonObject', defaultCaughtPokemonObject);
-  const pokemonInputHandler = (e) => {
-    const currentNumber = e.target.attributes['data-value'];
+  const [caughtPokemons, setCaughtPokemons] = useLocalStorage(
+    "caughtPokemonObject",
+    defaultCaughtPokemonObject
+  );
+  const pokemonInputHandler = e => {
+    const currentNumber = e.target.attributes["data-value"];
     const currentNumberValue = currentNumber.value;
     const tempCaughtPokemons = [...caughtPokemons];
     if (!tempCaughtPokemons.includes(currentNumber.value)) {
@@ -23,18 +26,54 @@ function App() {
       }
       setCaughtPokemons([...tempCaughtPokemons]);
     }
-  }
+  };
+  const [filterType, setFilterType] = useState("all");
+
+  const onChangeFilterHandler = e => {
+    const target = e.target;
+    setFilterType(target.id);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        Pokedex Living Dex Tracker
-      </header>
-      <PokedexTracker
-        data={pokdexJson}
-        currentCaughtPokemon={caughtPokemons}
-        inputHandler={pokemonInputHandler}
-      />
+      <header className="App-header">Pokedex Living Dex Tracker</header>
+      <main>
+        <div>
+          <input
+            id="all"
+            type="radio"
+            name="caught-type"
+            value={filterType}
+            checked={filterType === "all"}
+            onChange={onChangeFilterHandler}
+          />
+          <label htmlFor="all">All</label>
+          <input
+            id="caught"
+            type="radio"
+            name="caught-type"
+            value={filterType}
+            checked={filterType === "caught"}
+            onChange={onChangeFilterHandler}
+          />
+          <label htmlFor="caught">Caught</label>
+          <input
+            id="uncaught"
+            type="radio"
+            name="caught-type"
+            value={filterType}
+            checked={filterType === "uncaught"}
+            onChange={onChangeFilterHandler}
+          />
+          <label htmlFor="uncaught">Uncaught</label>
+        </div>
+        <PokedexTracker
+          data={pokdexJson}
+          filterType={filterType}
+          currentCaughtPokemon={caughtPokemons}
+          inputHandler={pokemonInputHandler}
+        />
+      </main>
     </div>
   );
 }
--- src/components/pokedex-tracker/index.js
+++ src/components/pokedex-tracker/index.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import './pokedex-tracker.css';
+import "./pokedex-tracker.css";
 
-function PokedexTracker({ data, currentCaughtPokemon, inputHandler }) {
+function PokedexTracker({
+  data,
+  filterType,
+  currentCaughtPokemon,
+  inputHandler
+}) {
   return (
-    <main className="pokedex-tracker">
-      {
-        data.map(pokemon => {
-          const labelId = `${pokemon.dexNumber}-${pokemon.dexName}`;
-          return (
-            <div
-              key={pokemon.dexNumber}
-              className="pokedex-tracker__item">
+    <div className="pokedex-tracker">
+      {data.map(pokemon => {
+        const labelId = `${pokemon.dexNumber}-${pokemon.dexName}`;
+        const isCaught = currentCaughtPokemon.includes(pokemon.dexNumber);
+
+        const currentItemVisible =
+          filterType === "all" ||
+          (filterType === "caught" && isCaught) ||
+          (filterType === "uncaught" && !isCaught);
+
+        return (
+          currentItemVisible && (
+            <div key={pokemon.dexNumber} className="pokedex-tracker__item">
               <input
                 data-value={pokemon.dexNumber}
                 id={labelId}
-                defaultChecked={currentCaughtPokemon.includes(pokemon.dexNumber)}
+                defaultChecked={isCaught}
                 type="checkbox"
                 onChange={inputHandler}
               />
               <div>{pokemon.dexNumber}</div>
               <div>
-                <label htmlFor={labelId}>
-                  {pokemon.dexName}
-                </label>
+                <label htmlFor={labelId}>{pokemon.dexName}</label>
               </div>
               <div>
-                {
-                  pokemon.dexTypes.map(type => {
-                    return (
-                      <div key={type}>
-                        {type}
-                      </div>
-                    )
-                  })
-                }
+                {pokemon.dexTypes.map(type => {
+                  return <div key={type}>{type}</div>;
+                })}
               </div>
             </div>
           )
-        })
-      }
-    </main>
-  )
+        );
+      })}
+    </div>
+  );
 }
 
 export default PokedexTracker;
